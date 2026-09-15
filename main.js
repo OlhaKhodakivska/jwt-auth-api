e980f26ea3f32163338795b16dc3cb69c07933ab
OlhaKhodakivska <olhakhodakivska@gmail.com>
2026-09-15 16:16:46 +0200

    Implement JWT authentication in login route and add dotenv for environment variable management

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({
@@ -13,6 +18,34 @@ app.get("/public", (req, res) => {
     res.json({
         message: "This is a public route",
     });
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username !== "admin" || password !== "geheim123") {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid credentials",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      userId: 1,
+      username: "admin",
+      role: "admin",
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "15m",
+    }
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
 });
 
 app.listen(PORT, () => {
